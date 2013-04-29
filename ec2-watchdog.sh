d7fe3ea56e98b4ff10ce5fa77c911e64ea47b209
Danilo Poccia <dpoccia@gmail.com>
2013-04-29 17:21:54 +0200

    First public release

--- ec2-watchdog.sh
+++ ec2-watchdog.sh
@@ -2,6 +2,10 @@
 # A simple script for Amazon EC2 to monitor another HA node and take over a Virtual IP (VIP)
 # if the service on the other node fails
 
+# The script is inspired by the following article:
+# - Leveraging Multiple IP Addresses for Virtual IP Address Fail-over in 6 Simple Steps
+#   http://aws.amazon.com/articles/2127188135977316
+
 # The two instances must have both two Network Interfaces and an Elastic IP on the primary interface
 # On the secondary interface there should be a single Elastic IP only on one of the two instances
 # That Elastic IP is the VIP moved between the instances and is identified by the AllocationID
